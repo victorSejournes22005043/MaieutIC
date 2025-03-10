f31a96acc4904cd9b9c7bdb0d54f8b460e2ac427
victorSejournes22005043 <victor.sejourne@etu.univ-amu.fr>
2025-03-10 15:22:28 +0100

    tailwind css installation

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
