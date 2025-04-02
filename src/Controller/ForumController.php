a9c35985275f62e28fefe149b07922ab89618c06
victorSejournes22005043 <victor.sejourne@etu.univ-amu.fr>
2025-04-02 16:15:01 +0200

    adds .env.exemple

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ForumRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ForumController extends AbstractController
 {
@@ -17,6 +18,20 @@ class ForumController extends AbstractController
 
         return $this->render('forum/forums.html.twig', [
             'forums' => $forums,
+        ]);
+    }
+
+    #[Route('/forums/{id}', name:'forum_show')]
+    public function show($id, ForumRepository $forumRepository): Response
+    {
+        $forum = $forumRepository->find($id);
+
+        if (!$forum) {
+            throw $this->createNotFoundException('Forum not found');
+        }
+
+        return $this->render('forum/forum.html.twig', [
+            'forum' => $forum,
         ]);
     }
 }
