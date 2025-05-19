75104d3ed4c5f0e13473668596ee70fce97a918b
LEPERLIER-Mael-21210975 <leperliermael@gmail.com>
2025-05-19 15:18:43 +0200

    wip : map page

--- src/Controller/MapsController.php
+++ src/Controller/MapsController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository;
 
 final class MapsController extends AbstractController{
     #[Route('/maps', name: 'app_maps')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
         return $this->render('maps/index.html.twig', [
             'controller_name' => 'MapsController',
+            'users' => $users,
         ]);
     }
 }
